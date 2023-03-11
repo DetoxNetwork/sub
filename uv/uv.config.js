0789393fa1d0d3de0948e272407bfb52765d8dc5
SupremeRubisco <92188403+SupremeRubisco@users.noreply.github.com>
2023-03-11 08:28:30 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,12 +1,12 @@
 /*global Ultraviolet*/
 self.__uv$config = {
-    prefix: '/uv/service/',
+    prefix: '/sub/uv/service/',
     bare: 'https://sublink-supremerubisco.koyeb.app/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/uv/uv.handler.js',
-    client: '/uv/uv.client.js',
-    bundle: '/uv/uv.bundle.js',
-    config: '/uv/uv.config.js',
-    sw: '/uv/uv.sw.js',
+    handler: 'https://raw.githubusercontent.com/DetoxNetwork/sub/main/uv/uv.handler.js',
+    client: 'https://raw.githubusercontent.com/DetoxNetwork/sub/main/uv/uv.client.js',
+    bundle: 'https://raw.githubusercontent.com/DetoxNetwork/sub/main/uv/uv.bundle.js',
+    config: '/sub/uv/uv.config.js',
+    sw: 'https://raw.githubusercontent.com/DetoxNetwork/sub/main/uv/uv.sw.js',
 };
